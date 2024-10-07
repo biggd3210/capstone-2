36bdf23752348c1aa19f9fa0d522ff8263a6cbd2
Derek Biggers <biggd3210@gmail.com>
2024-10-07 13:00:04 -0400

    backend resolve page redirect after successful form submission

--- back-end/models/document.js
+++ back-end/models/document.js
@@ -16,7 +16,7 @@ class Document {
      * authorization: signed in, User must be authorized for facility. 
     */
 
-    static async create({ id, author, docType, attachments, facility, period, year, fileName, dateTime }) {
+    static async create({ id, author, docType, attachments, facility, period, year, fileName, dateTime, dueDate }) {
         const duplicateCheck = await db.query(
             `SELECT id
             FROM documents
@@ -30,10 +30,10 @@ class Document {
 
         const result = await db.query(
             `INSERT INTO documents
-            (id, author, doc_type, image_components, facility_id, doc_period, doc_year, file_name, date_time)
-            VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)
-            RETURNING id, author, doc_type AS "docType", image_components AS "imageComponents", facility_id AS "facilityId", doc_period AS "docPeriod", doc_year AS "docYear", file_name AS "fileName", date_time AS "dateTime"`,
-            [ id, author, docType, attachments, facility, period, year, fileName, dateTime ],
+            (id, author, doc_type, image_components, facility_id, doc_period, doc_year, file_name, date_time, due_date)
+            VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)
+            RETURNING id, author, doc_type AS "docType", image_components AS "imageComponents", facility_id AS "facilityId", doc_period AS "docPeriod", doc_year AS "docYear", file_name AS "fileName", date_time AS "dateTime", due_date AS "dueDate"`,
+            [ id, author, docType, attachments, facility, period, year, fileName, dateTime, dueDate ],
         );
 
         const document = result.rows[0];
